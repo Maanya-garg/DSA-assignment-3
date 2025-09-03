e287725b7db5d06ae24a81c73dfb420ee7fa8f72
Maanya-garg <maanya.grg@gmail.com>
2025-09-03 22:17:42 +0530

    assg 3,addques4.cpp

--- assg 3,addques4.cpp
+++ assg 3,addques4.cpp
@@ -1,18 +1,22 @@
 #include<iostream>
+#include<stack>
 using namespace std;
+std::stack<int> s;
 void greatestelement(int arr[],int n){
-		for (int i=0;i<n;i++){
-			bool found=false;
-		for (int j=i+1;j<n;j++){
-			if(arr[j]>arr[i]){
-				cout<< "for"<<arr[i]<< "the number of days are"<<(j-i)<<endl;
-				found=true;
-				break;
-			}
-		}
-		if (!found){
-			cout<< "for"<<arr[i]<<"the number of days are 0"<<endl;
-		}
+	int result[n];
+for (int i = n-1; i >=0 ; i--) {
+        while (!s.empty() && arr[s.top()] <= arr[i]) {
+            s.pop();
+        }
+        if (s.empty()) {
+            result[i]=0;
+        } else {
+            result[i]=s.top()-i;
+        }
+        s.push(i);
+    }
+    for (int i=0;i<n;i++){
+    	cout<<result[i]<<" ";
 	}
 }
 int main(){
